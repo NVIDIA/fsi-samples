61f48a76789c599017b16633dc0ded79aef8f57c
Yi Dong <yidong@nvidia.com>
2019-06-19 12:57:51 -0700

    added unit tests for the cuindicator

---
+++ tests/unit/test_pewm.py
@@ -0,0 +1,105 @@
+'''
+Workflow Serialization Unit Tests
+
+To run unittests:
+
+# Using standard library unittest
+
+python -m unittest -v
+python -m unittest tests/unit/test_pewm.py -v
+
+or
+
+python -m unittest discover <test_directory>
+python -m unittest discover -s <directory> -p 'test_*.py'
+
+# Using pytest
+# "conda install pytest" or "pip install pytest"
+pytest -v tests
+pytest -v tests/unit/test_pewm.py
+
+'''
+import os
+import warnings
+from io import StringIO
+import shutil, tempfile
+import pandas as pd
+import unittest
+import pathlib
+import cudf
+from gquant.cuindicator import PEwm
+import numpy as np
+# --------------------------------------------------------- Keep tests in order
+def make_orderer():
+    order = {}
+
+    def ordered(f):
+        order[f.__name__] = len(order)
+        return f
+
+    def compare(a, b):
+        return [1, -1][order[a] < order[b]]
+
+    return ordered, compare
+
+ordered, compare = make_orderer()
+unittest.defaultTestLoader.sortTestMethodsUsing = compare
+
+# ------------------------------------------- Workflow Serialization Test Cases
+
+def error_function(gpu_arr, result_series):
+    gpu_arr = gpu_arr.to_array(fillna='pandas')
+    pan_arr = result_series.values
+    gpu_arr = gpu_arr[~np.isnan(gpu_arr) & ~np.isinf(gpu_arr)]
+    pan_arr = pan_arr[~np.isnan(pan_arr) & ~np.isinf(pan_arr)]
+    err = np.abs(gpu_arr - pan_arr).max()
+    return err
+
+class TestIndicator(unittest.TestCase):
+
+    def setUp(self):
+        random_array = np.arange(20, dtype=np.float64)
+        indicator = np.zeros(20, dtype=np.int32)
+        indicator[0]=1
+        indicator[10]=1
+        df = cudf.dataframe.DataFrame()
+        df['in'] = random_array
+        df['indicator'] = indicator
+
+        pdf = pd.DataFrame()
+        pdf['in0'] = random_array[0:10]
+        pdf['in1'] = random_array[10:]
+
+        # ignore importlib warnings.
+        self._pandas_data = pdf
+        self._cudf_data = df
+
+    def tearDown(self):
+        pass
+
+    @ordered
+    def test_pewm(self):
+        '''Test portfolio ewm method'''
+        self._cudf_data['ewma'] = PEwm(3,
+                                       self._cudf_data['in'],
+                                       self._cudf_data[
+                                           'indicator'].data.to_gpu_array(),
+                                       thread_tile=2,
+                                       number_of_threads=2).mean()
+        gpu_array = self._cudf_data['ewma']
+        gpu_result = gpu_array[0:10]
+        cpu_result = self._pandas_data['in0'].ewm(span=3,
+                                                  min_periods=3).mean()
+        err = error_function(gpu_result, cpu_result)
+        msg = "bad error %f\n" % (err,)
+        self.assertTrue(np.isclose(err, 0, atol=1e-6), msg)
+
+        cpu_result = self._pandas_data['in1'].ewm(span=3,
+                                                  min_periods=3).mean()
+        gpu_result = gpu_array[10:20]
+        err = error_function(gpu_result, cpu_result)
+        msg = "bad error %f\n" % (err,)
+        self.assertTrue(np.isclose(err, 0, atol=1e-6), msg)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unit/test_util.py
@@ -0,0 +1,101 @@
+'''
+Workflow Serialization Unit Tests
+
+To run unittests:
+
+# Using standard library unittest
+
+python -m unittest -v
+python -m unittest tests/unit/test_util.py -v
+
+or
+
+python -m unittest discover <test_directory>
+python -m unittest discover -s <directory> -p 'test_*.py'
+
+# Using pytest
+# "conda install pytest" or "pip install pytest"
+pytest -v tests
+pytest -v tests/unit/test_util.py
+
+'''
+import os
+import warnings
+from io import StringIO
+import shutil, tempfile
+import pandas as pd
+import unittest
+import pathlib
+import cudf
+from gquant.cuindicator import shift, diff
+import numpy as np
+# --------------------------------------------------------- Keep tests in order
+def make_orderer():
+    order = {}
+
+    def ordered(f):
+        order[f.__name__] = len(order)
+        return f
+
+    def compare(a, b):
+        return [1, -1][order[a] < order[b]]
+
+    return ordered, compare
+
+ordered, compare = make_orderer()
+unittest.defaultTestLoader.sortTestMethodsUsing = compare
+
+# ------------------------------------------- Workflow Serialization Test Cases
+
+def error_function(gpu_arr, result_series):
+    gpu_arr = gpu_arr.to_array(fillna='pandas')
+    pan_arr = result_series.values
+    gpu_arr = gpu_arr[~np.isnan(gpu_arr) & ~np.isinf(gpu_arr)]
+    pan_arr = pan_arr[~np.isnan(pan_arr) & ~np.isinf(pan_arr)]
+    err = np.abs(gpu_arr - pan_arr).max()
+    return err
+
+class TestIndicator(unittest.TestCase):
+
+    def setUp(self):
+        array_len = int(1e4)
+        self.average_window = 300
+        number_type = np.float64
+        random_array = np.random.rand(array_len)
+
+        df = cudf.dataframe.DataFrame()
+        df['in'] = random_array
+
+        pdf = pd.DataFrame()
+        pdf['in'] = random_array
+
+        # ignore importlib warnings.
+        self._pandas_data = pdf
+        self._cudf_data = df
+
+    def tearDown(self):
+        pass
+
+    @ordered
+    def test_diff_functions(self):
+        '''Test diff method'''
+        for window in [-1, -2, -3, 1, 2, 3]:
+            gpu_result = diff(self._cudf_data['in'], window)
+            cpu_result = self._pandas_data['in'].diff(window)
+            err = error_function(cudf.Series(gpu_result), cpu_result)
+            msg = "bad error %f\n" % (err,)
+            self.assertTrue(np.isclose(err, 0, atol=1e-6), msg)
+
+    @ordered
+    def test_shift_functions(self):
+        '''Test shift method'''
+        for window in [-1, -2, -3, 1, 2, 3]:
+            gpu_result = shift(self._cudf_data['in'], window)
+            cpu_result = self._pandas_data['in'].shift(window)
+            err = error_function(cudf.Series(gpu_result), cpu_result)
+            msg = "bad error %f\n" % (err,)
+            self.assertTrue(np.isclose(err, 0, atol=1e-6), msg)
+
+
+if __name__ == '__main__':
+    unittest.main()
