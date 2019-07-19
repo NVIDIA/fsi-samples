fixed the indicator node computation

gquant/plugin_nodes/transform/indicatorNode.py
@@ -1,5 +1,5 @@
 from gquant.dataframe_flow import Node
-# from .returnFeatureNode import ReturnFeatureNode
+import numpy as np
 import gquant.cuindicator as ci
 
 
@@ -71,11 +71,13 @@ class IndicatorNode(Node):
                 input_df[out_col] = v
                 out_cols.append(out_col)
         # remove all the na elements, requires cudf>=0.8
+        # import pudb
+        # pudb.set_trace()
         if "remove_na" in self.conf and self.conf["remove_na"]:
             na_element = input_df[out_cols[0]].isna()
             for i in range(1, len(out_cols)):
                 na_element |= input_df[out_cols[i]].isna()
-            input_df = input_df[~na_element]
+            input_df = input_df.iloc[np.where((~na_element).to_array())[0]]
         return input_df
 
 