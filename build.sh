7dbb77e934214bb54b8e8df349d42b6e9a5ef5a8
Miguel Martínez <miguelm@nvidia.com>
2019-07-31 00:35:34 +0100

    Update to RAPIDS v0.8 container

--- build.sh
+++ build.sh
@@ -1,65 +1,39 @@
 D_FILE=${D_FILE:='Dockerfile.Rapids'}
 D_CONT=${D_CONT:='gquant/gquant:latest'}
 
-cat > $D_FILE <<EOF
-FROM nvcr.io/nvidia/rapidsai/rapidsai:0.7-cuda10.0-devel-ubuntu18.04-gcc7-py3.6
+echo "Fetching latest version of gQuant project"
+git clone --recursive https://github.com/rapidsai/gQuant
 
+cat > $D_FILE <<EOF
+FROM nvcr.io/nvidia/rapidsai/rapidsai:cuda9.2-runtime-ubuntu16.04
 USER root
 
-RUN apt-get update
-RUN apt-get install -y libfontconfig1 libxrender1
+ADD gQuant /rapids/gQuant
+
+RUN apt-get update && apt-get install -y libfontconfig1 libxrender1
 
 SHELL ["bash","-c"]
+
 #
 # Additional python libs
 #
-RUN source activate rapids \
-    && pip install cython matplotlib networkx nxpd graphviz pudb
-
-RUN cd /rapids && source activate rapids \
-    && conda install -c conda-forge bqplot nodejs \
-    && conda install -y python-graphviz\
-    && conda install -y tqdm \
-    && conda install -y pytables \
-    && conda install -y -f mkl \
-    && conda install -y numpy scipy scikit-learn numexpr 
-    ## && conda install -c nvidia -c rapidsai -c numba -c conda-forge -c defaults cudf=0.6 python=3.6 cudatoolkit=10.0
-    ## && conda install -c rapidsai cudf \
-    ## && conda install -c rapidsai cuml \
-    ## && git clone https://github.com/rapidsai/cuml.git
+RUN pip install nxpd graphviz pudb dask_labextension sphinx sphinx_rtd_theme recommonmark numpydoc cupy-cuda92
+RUN conda install -y -c conda-forge python-graphviz bqplot=0.11.5 nodejs=11.11.0 jupyterlab=0.35.4 \
+    ipywidgets=7.4.2 pytables mkl numexpr
 
 #
 # required set up
 #
-RUN source activate rapids \
-    && /conda/envs/rapids/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager \
-    && /conda/envs/rapids/bin/jupyter labextension install bqplot \
-    && mkdir /.local        \
-    && chmod 777 /.local    \
-    && mkdir /.jupyter      \
-    && chmod 777 /.jupyter  \
-    && mkdir /.config       \
-    && chmod 777 /.config   \
-    && mkdir /.cupy         \
-    && chmod 777 /.cupy
-
-RUN source activate rapids  \
-   && pip install dask_labextension \
-   && pip install sphinx sphinx_rtd_theme recommonmark numpydoc \
-   && /conda/envs/rapids/bin/jupyter labextension install dask-labextension \
-   && pip install cupy-cuda100
+RUN jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.38.1 \
+    && jupyter labextension install bqplot@0.4.5 \
+    && mkdir /.local /.jupyter /.config /.cupy  \
+    && chmod 777 /.local /.jupyter /.config /.cupy
 
 EXPOSE 8888
 EXPOSE 8787
 EXPOSE 8786
 
-# the addon for vim editor
-# RUN source activate rapids  \
-#    && /conda/envs/rapids/bin/jupyter labextension install jupyterlab_vim
-
-
 WORKDIR /
 EOF
 
 docker build -f $D_FILE -t $D_CONT .
-
