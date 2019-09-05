1459ff9e8eee164adb934225c190b22da3f44ed5
Yi Dong <yidong@nvidia.com>
2019-09-05 12:16:14 -0700

    clean up the documents

--- gquant/cuindicator/__init__.py
+++ gquant/cuindicator/__init__.py
@@ -1,9 +1,11 @@
 from .ewm import Ewm
-from .indicator import *  # noqa: F403
+from .indicator import *  # noqa: F403, F401
 from .pewm import PEwm
 from .rolling import Rolling
 from .util import (shift, diff, substract, summation,
                    multiply, division, scale, cumsum)
+from .frac_diff import fractional_diff, get_weights_floored
 
 __all__ = ["Ewm", "PEwm", "Rolling", "shift", "diff", "substract",
-           "summation", "multiply", "division", "scale", "cumsum"]
+           "summation", "multiply", "division", "scale", "cumsum",
+           "fractional_diff", "get_weights_floored"]
--- gquant/cuindicator/frac_diff.py
+++ gquant/cuindicator/frac_diff.py
@@ -38,8 +38,33 @@ def get_weights_floored(d, num_k, floor=1e-3):
 def conv_window(shared, history_len, out_arr, window_size,
                 arr_len, offset, offset2, min_size):
     """
-    This function is to compute the sum for the window
-    See `window_kernel` for detailed arguments
+    This function is to do convolution for one thread
+
+    Arguments:
+    ------
+     shared: numba.cuda.DeviceNDArray
+        3 chunks of data are stored in the shared memory
+        the first [0, window_size) elements is the chunk of data that is
+        necessary to compute the first convolution element.
+        then [window_size, window_size + thread_tile * blockDim) elements
+        are the inputs allocated for this block of threads
+        the last [window_size + thread_tile,
+        window_size + thread_tile + window_size) is to store the kernel values
+     history_len: int
+        total number of historical elements available for this chunk of data
+     out_arr: numba.cuda.DeviceNDArray
+        output gpu_array of size of `thread_tile`
+     window_size: int
+        the number of elements in the kernel
+     arr_len: int
+        the chunk array length, same as `thread_tile`
+     offset: int
+        indicate the starting index of the chunk array in the shared for
+        this thread.
+     offset: int
+        indicate the starting position of the weights/kernel array
+     min_size: int
+         the minimum number of non-na elements
     """
     for i in range(arr_len):
         if i + history_len < window_size-1:
@@ -60,21 +85,28 @@ def conv_window(shared, history_len, out_arr, window_size,
 
 
 @cuda.jit
-def kernel(in_arr, weight_arr, out_arr, backward_length,
+def kernel(in_arr, weight_arr, out_arr, window,
            arr_len, thread_tile, min_size):
     """
-    This kernel is to copy input array elements into shared array.
-    The total window size is backward_length + forward_length. To compute
-    output element at i, it uses [i - backward_length - 1, i] elements in
-    history, and [i + 1, i + forward_lengh] elements in the future.
+    This kernel is to do 1D convlution on `in_arr` array with `weight_arr`
+    as kernel. The results is saved on `out_arr`.
+
     Arguments:
-        in_arr: input gpu array
-        out_arr: output gpu_array
-        backward_length: the history elements in the windonw
-        arr_len: the input/output array length
-        thread_tile: each thread is responsible for `thread_tile` number
-                     of elements
-        min_size: the minimum number of non-na elements
+    ------
+     in_arr: numba.cuda.DeviceNDArray
+        input gpu array
+     weight_arr: numba.cuda.DeviceNDArray
+        convolution kernel gpu array
+     out_arr: numba.cuda.DeviceNDArray
+        output gpu_array
+     window: int
+        the number of elements in the weight_arr
+     arr_len: int
+        the input/output array length
+     thread_tile: int
+        each thread is responsible for `thread_tile` number of elements
+     min_size: int
+         the minimum number of non-na elements
     """
     shared = cuda.shared.array(shape=0,
                                dtype=numba.float64)
@@ -88,28 +120,28 @@ def kernel(in_arr, weight_arr, out_arr, backward_length,
     for j in range(thread_tile):
         offset = tx + j * block_size
         if (starting_id + offset) < arr_len:
-            shared[offset + backward_length - 1] = in_arr[
+            shared[offset + window - 1] = in_arr[
                 starting_id + offset]
         cuda.syncthreads()
 
-    # copy the backward_length - 1 into the shared
-    for j in range(0, backward_length - 1, block_size):
+    # copy the window - 1 into the shared
+    for j in range(0, window - 1, block_size):
         if (((tx + j) <
-             backward_length - 1) and (
-                 starting_id - backward_length + 1 + tx + j >= 0)):
+             window - 1) and (
+                 starting_id - window + 1 + tx + j >= 0)):
             shared[tx + j] = \
-                in_arr[starting_id - backward_length + 1 + tx + j]
+                in_arr[starting_id - window + 1 + tx + j]
         cuda.syncthreads()
     # copy the weights into the shared
-    for j in range(0, backward_length, block_size):
+    for j in range(0, window, block_size):
         element_id = tx + j
-        if (((tx + j) < backward_length) and (element_id < backward_length)):
-            shared[thread_tile * block_size + backward_length - 1 + tx +
+        if (((tx + j) < window) and (element_id < window)):
+            shared[thread_tile * block_size + window - 1 + tx +
                    j] = weight_arr[tx + j]
         cuda.syncthreads()
     # slice the shared memory for each threads
     start_shared = tx * thread_tile
-    his_len = min(backward_length - 1,
+    his_len = min(window - 1,
                   starting_id + tx * thread_tile)
     # slice the global memory for each threads
     start = starting_id + tx * thread_tile
@@ -117,84 +149,93 @@ def kernel(in_arr, weight_arr, out_arr, backward_length,
     sub_outarr = out_arr[start:end]
     sub_len = end - start
     conv_window(shared, his_len, sub_outarr,
-                backward_length, sub_len,
-                backward_length - 1 + start_shared,
-                thread_tile * block_size + backward_length - 1,
+                window, sub_len,
+                window - 1 + start_shared,
+                thread_tile * block_size + window - 1,
                 min_size)
 
 
-class FracDiff(object):
+def fractional_diff(input_arr, d=0.5, floor=1e-3, min_periods=None,
+                    thread_tile=48, number_of_threads=1024):
+    """
+    The fractional difference computation method.
 
-    def __init__(self, input_arr, d=0.5, floor=1e-3, min_periods=None,
-                 thread_tile=48, number_of_threads=64):
-        """
-        The Frac Diff class that is used to do compute the fractional
-        difference
+    Arguments:
+    -------
+      input_arr: numba.cuda.DeviceNDArray or cudf.Series
+        the input array to compute the fractional difference
+      d: float
+        the differencing value. range from 0 to 1
+      floor: float
+        minimum value for the weights for computational efficiency.
+      min_periods: int
+        default the lengths of the weights. Need at least min_periods of
+        non-na elements to get fractional difference value
+      thread_tile: int
+        each thread will be responsible for `thread_tile` number of
+        elements in window computation
+      number_of_threads: int
+        number of threads in a block for CUDA computation
 
-        Arguments:
-            input_arr: the input GPU array or cudf.Series
-            d: the fraction number
-            floor: the cut off threashold to compute the fractional weights
-            thread_tile: each thread will be responsible for `thread_tile`
-                         number of elements in window computation
-            number_of_threads: num. of threads in a block for CUDA computation
-        """
-        if isinstance(input_arr, numba.cuda.cudadrv.devicearray.DeviceNDArray):
-            self.gpu_in = input_arr
-        else:
-            self.gpu_in = input_arr.data.to_gpu_array()
+    Returns
+    -------
+    numba.cuda.DeviceNDArray
+        the computed fractional difference array
 
-        self.weights = get_weights_floored(d=d,
-                                           num_k=len(input_arr),
-                                           floor=floor)[::-1, 0]
-        self.weights = numba.cuda.to_device(np.ascontiguousarray(self.weights))
-        self.window = len(self.weights)
-        if min_periods is None:
-            self.min_periods = self.window
-        else:
-            self.min_periods = min_periods
-        self.number_of_threads = number_of_threads
-        self.array_len = len(self.gpu_in)
-        self.gpu_out = numba.cuda.device_array_like(self.gpu_in)
-        self.thread_tile = thread_tile
-        self.number_of_blocks = \
-            (self.array_len + (number_of_threads * thread_tile - 1)) // \
-            (number_of_threads * thread_tile)
+    """
+    if isinstance(input_arr, numba.cuda.cudadrv.devicearray.DeviceNDArray):
+        gpu_in = input_arr
+    else:
+        gpu_in = input_arr.data.to_gpu_array()
 
-        self.shared_buffer_size = (self.number_of_threads * self.thread_tile +
-                                   self.window - 1 + self.window)
+    # compute the weights for the fractional difference
+    weights = get_weights_floored(d=d,
+                                  num_k=len(input_arr),
+                                  floor=floor)[::-1, 0]
+    weights = numba.cuda.to_device(np.ascontiguousarray(weights))
 
-    def compute(self):
-        gpu_out = numba.cuda.device_array_like(self.gpu_in)
-        # gpu_out = cudf.Series(gpu_out)
-        kernel[(self.number_of_blocks,),
-               (self.number_of_threads,),
-               0,
-               self.shared_buffer_size * 8](self.gpu_in,
-                                            self.weights,
-                                            gpu_out,
-                                            self.window,
-                                            self.array_len,
-                                            self.thread_tile,
-                                            self.min_periods)
-        # numba.cuda.synchronize()
-        return gpu_out
+    window = len(weights)
+
+    if min_periods is None:
+        min_periods = window
+    else:
+        min_periods = min_periods
+
+    number_of_threads = number_of_threads
+    array_len = len(gpu_in)
+
+    # allocate the output array
+    gpu_out = numba.cuda.device_array_like(gpu_in)
+
+    number_of_blocks = \
+        (array_len + (number_of_threads * thread_tile - 1)) // \
+        (number_of_threads * thread_tile)
+
+    shared_buffer_size = (number_of_threads * thread_tile +
+                          window - 1 + window)
+
+    # call the conv kernel
+    kernel[(number_of_blocks,),
+           (number_of_threads,),
+           0,
+           shared_buffer_size * 8](gpu_in,
+                                   weights,
+                                   gpu_out,
+                                   window,
+                                   array_len,
+                                   thread_tile,
+                                   min_periods)
+    return gpu_out
+
 
 np.random.seed(3)
 num = np.random.rand(10000000)
 arr = cuda.to_device(num)
-start = time.time()
-f = FracDiff(arr, d=0.3, floor=1e-4, thread_tile=2, number_of_threads=1024)
-fnum = f.compute()
-end = time.time()
+f = fractional_diff(arr, d=0.3, floor=1e-4, thread_tile=2, number_of_threads=1024)
 
-print(f'compile Time {end-start} s')
+#print(f'compile Time {end-start} s')
 
-start = time.time()
-f = FracDiff(arr, d=0.5, floor=1e-4, thread_tile=2, number_of_threads=1024)
-fnum = f.compute()
-end = time.time()
+f = fractional_diff(arr, d=0.5, floor=1e-4, thread_tile=2, number_of_threads=1024)
 
-print(f'no compile Time {end-start} s')
-
+#print(f'no compile Time {end-start} s')
 
