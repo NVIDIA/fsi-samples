b1a091bc18e65070bc567b17c2ef8ce21418caca
Yi Dong <yidong@nvidia.com>
2020-01-17 09:10:52 -0800

    updated to RAPIDS 10.1

--- docker/build.sh
+++ docker/build.sh
@@ -2,55 +2,60 @@
 
 echo "Building gQuant container..."
 
-echo -e "Please, select the option which better fits your system configuration:\n" \
-        " - '1' for Ubuntu 16.04 + cuda 9.2\n" \
-        " - '2' for Ubuntu 16.04 + cuda 10.0\n" \
-        " - '3' for Ubuntu 18.04 + cuda 9.2\n" \
-        " - '4' for Ubuntu 18.04 + cuda 10.0"
+echo -e "\nPlease, select your operating system:\n" \
+     " - '1' for Ubuntu 16.04\n" \
+     " - '2' for Ubuntu 18.04\n" \
+     " - '3' for CentOS"
 
-read -p "Enter your option and hit return [1]-4: " SYSTEM_CONFIGURATION
+read -p "Enter your option and hit return [1]-3: " OPERATING_SYSTEM
 
-SYSTEM_CONFIGURATION=${SYSTEM_CONFIGURATION:-1}
-case $SYSTEM_CONFIGURATION in
+OPERATING_SYSTEM=${OPERATING_SYSTEM:-1}
+case $OPERATING_SYSTEM in
     2)
-        echo "Ubuntu 16.04 + cuda 10.0 selected."
-        OS_STR='16.04'
-        CONTAINER_VER='10.0'
-        CUPY='cupy-cuda100'
-        ;;
+	echo "Ubuntu 18.04 selected."
+	OS_STR="ubuntu18.04"
+	;;
     3)
-        echo "Ubuntu 18.04 + cuda 9.2 selected."
-        OS_STR='18.04'
-        CONTAINER_VER='9.2'
-        CUPY='cupy-cuda92'
-        ;;
-    4)
-        echo "Ubuntu 18.04 + cuda 10.0 selected."
-        OS_STR='18.04'
-	CONTAINER_VER='10.0'
-	CUPY='cupy-cuda100'
-        ;;
+	echo "CentOS selected."
+	OS_STR="centos7"
+	;;
     *)
-        echo "Ubuntu 16.04 + cuda 9.2 selected."
-        OS_STR='16.04'
-        CONTAINER_VER='9.2'
-        CUPY='cupy-cuda92'
-        ;;
+	echo "Ubuntu 16.04 selected."
+	OS_STR="ubuntu16.04"
+	;;
 esac
 
-CONTAINER="nvcr.io/nvidia/rapidsai/rapidsai:0.9-cuda${CONTAINER_VER}-runtime-ubuntu${OS_STR}"
+echo -e "\nPlease, select your cuda version:\n" \
+     " - '1' for cuda 9.2\n" \
+     " - '2' for cuda 10.0\n" \
+     " - '3' for cuda 10.1.2"
 
-read -p "Would you like to install Vim JupyterLab Extension (optional) [N]/y: " VIM_INSTALL
+read -p "Enter your option and hit return [1]-3: " CUDA_VERSION
 
-VIM_INSTALL=${VIM_INSTALL:-N}
-if [ "$VIM_INSTALL" = "Y" ] || [ "$VIM_INSTALL" = "y" ]; then
-    echo "Vim JupyterLab Extension will be installed."
-else
-    echo "Vim JupyterLab Extension will not be installed."
-fi
+CUDA_VERSION=${CUDA_VERSION:-1}
+case $CUDA_VERSION in
+    2)
+	echo "cuda 10.0 selected."
+	CONTAINER_VER='10.0'
+	CUPY='cupy-cuda100'
+	;;
+    3)
+	echo "cuda 10.1.2 selected."
+	CONTAINER_VER='10.1'
+	CUPY='cupy-cuda101'
+	;;
+    *)
+	echo "cuda 9.2 selected."
+	CONTAINER_VER='9.2'
+	CUPY='cupy-cuda92'
+	;;
+esac
+
+CONTAINER="nvcr.io/nvidia/rapidsai/rapidsai:0.11-cuda${CONTAINER_VER}-runtime-${OS_STR}"
+
 
 D_FILE=${D_FILE:='Dockerfile.Rapids'}
-D_CONT=${D_CONT:='gquant/gquant:latest'}
+D_CONT=${D_CONT:="gquant/gquant:${OS_STR}"}
 
 mkdir -p gQuant
 cp -r ../gquant ./gQuant
@@ -63,27 +68,26 @@ rsync -av --progress ../notebooks ./gQuant --exclude data --exclude .cache --exc
 cat > $D_FILE <<EOF
 FROM $CONTAINER
 USER root
-
 ADD ./gQuant /rapids/gQuant
-
-RUN apt-get update && apt-get install -y libfontconfig1 libxrender1
-
+RUN if [ "$OS_STR" = "centos7" ]; then \
+        yum install -y fontconfig-devel libXrender ; \
+    else \
+        apt-get update && apt-get install -y libfontconfig1 libxrender1 ; \
+    fi
+# RUN apt-get update && apt-get install -y libfontconfig1 libxrender1
 SHELL ["bash","-c"]
-
 #
 # Additional python libs
 #
 RUN source activate rapids \ 
-    && pip install nxpd $CUPY 
-
+    && pip install $CUPY
 RUN source activate rapids \
     && cd /rapids/gQuant \
     && pip install .
-
 RUN source activate rapids \ 
     && conda install -y -c conda-forge dask-labextension recommonmark numpydoc sphinx_rtd_theme pudb \
-    python-graphviz bqplot=0.11.5 nodejs=11.11.0 jupyterlab=0.35.4 ipywidgets=7.4.2 pytables mkl numexpr
-
+    python-graphviz bqplot=0.11.5 nodejs=11.11.0 jupyterlab=0.35.4 ipywidgets=7.4.2 pytables mkl numexpr \
+    pydot
 #
 # required set up
 #
@@ -92,16 +96,11 @@ RUN source activate rapids \
     && jupyter labextension install bqplot@0.4.5 --no-build \
     && mkdir /.local /.jupyter /.config /.cupy  \
     && chmod 777 /.local /.jupyter /.config /.cupy
-
-RUN if [ "$VIM_INSTALL" = "Y" ] || [ "$VIM_INSTALL" = "y" ]; then /conda/envs/rapids/bin/jupyter labextension install jupyterlab_vim@0.10.1 --no-build ; fi
-
 RUN source activate rapids \ 
     && jupyter lab build && jupyter lab clean
-
 EXPOSE 8888
 EXPOSE 8787
 EXPOSE 8786
-
 WORKDIR /rapids
 EOF
 
