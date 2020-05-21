4523772313ce738478840ba38a1132289046bb8d
Yi Dong <yidong@nvidia.com>
2020-05-21 15:27:43 -0700

    fixed the indicator plot

--- notebooks/cuIndicator/viz/bollinger_bands.py
+++ notebooks/cuIndicator/viz/bollinger_bands.py
@@ -6,13 +6,15 @@ from gquant.cuindicator import bollinger_bands as indicator_fun
 
 def get_para_widgets():
     para_selector = widgets.IntSlider(min=2, max=60, description="Bollinger Bands")
-    para_selector_widgets = [para_selector]                    
+    para_selector_widgets = [para_selector]
     return para_selector_widgets
 
 def get_parameters(stock_df, para_selector_widgets):
     return  (stock_df["close"],) + tuple([w.value for w in para_selector_widgets])
 
 def process_outputs(output, stock_df):
+    output.index = stock_df.index
+
     stock_df['out0'] = output.b1
     stock_df['out0'] = stock_df['out0'].fillna(0)
     stock_df['out1'] = output.b2
@@ -24,11 +26,11 @@ def create_figure(stock, dt_scale, sc, color_id, f, indicator_figure_height, fig
     sc_co2 = LinearScale()
     ax_y = Axis(label='Bollinger b1', scale=sc_co, orientation='vertical')
     ax_y2 = Axis(label='Bollinger b2', scale=sc_co2, orientation='vertical', side='right')
-    new_line = Lines(x=stock.datetime, y=stock['out0'], scales={'x': dt_scale, 'y': sc_co}, colors=[CATEGORY20[color_id[0]]]) 
-    new_line2 = Lines(x=stock.datetime, y=stock['out1'], scales={'x': dt_scale, 'y': sc_co2}, colors=[CATEGORY20[(color_id[0] + 1) % len(CATEGORY20)]]) 
+    new_line = Lines(x=stock.datetime, y=stock['out0'], scales={'x': dt_scale, 'y': sc_co}, colors=[CATEGORY20[color_id[0]]])
+    new_line2 = Lines(x=stock.datetime, y=stock['out1'], scales={'x': dt_scale, 'y': sc_co2}, colors=[CATEGORY20[(color_id[0] + 1) % len(CATEGORY20)]])
     new_fig = Figure(marks=[new_line, new_line2], axes=[ax_y, ax_y2])
     new_fig.layout.height = indicator_figure_height
-    new_fig.layout.width = figure_width                    
+    new_fig.layout.width = figure_width
     figs = [new_line, new_line2]
     add_new_indicator(new_fig)
     return figs
@@ -38,6 +40,6 @@ def update_figure(stock, objects):
     line2 = objects[1]
     with line.hold_trait_notifications() as lc, line2.hold_trait_notifications() as lc2:
         line.y = stock['out0']
-        line.x = stock.datetime        
+        line.x = stock.datetime
         line2.y = stock['out1']
-        line2.x = stock.datetime      
+        line2.x = stock.datetime
--- notebooks/cuIndicator/viz/keltner_channel.py
+++ notebooks/cuIndicator/viz/keltner_channel.py
@@ -12,7 +12,9 @@ def get_para_widgets():
 def get_parameters(stock_df, para_selector_widgets):
     return (stock_df["high"], stock_df["low"], stock_df["close"]) + tuple([w.value for w in para_selector_widgets])
 
-def process_outputs(output,stock_df):
+def process_outputs(output, stock_df):
+    output.index = stock_df.index
+
     stock_df['out'] = output
     stock_df['out'] = output.fillna(0)
     return stock_df
@@ -23,7 +25,7 @@ def create_figure(stock, dt_scale, sc, color_id, f, indicator_figure_height, fig
     new_line = Lines(x=stock.datetime, y=stock['out'], scales={'x': dt_scale, 'y': sc_co}, colors=[CATEGORY20[color_id[0]]])
     new_fig = Figure(marks=[new_line], axes=[ax_y])
     new_fig.layout.height = indicator_figure_height
-    new_fig.layout.width = figure_width                    
+    new_fig.layout.width = figure_width
     figs = [new_line]
     # add new figure
     add_new_indicator(new_fig)
