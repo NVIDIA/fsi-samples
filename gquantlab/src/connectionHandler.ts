0d3052c537cd58c007c516ca9918b17e75f25251
Yi Dong <doyend@gmail.com>
2020-07-14 19:16:15 -0400

    converted some convinent funs from js to ts

---
+++ gquantlab/src/connectionHandler.ts
@@ -0,0 +1,139 @@
+import * as d3 from 'd3';
+import { validConnection } from './validator';
+import { IEdge } from './document';
+
+export function handleMouseUp(that: any) {
+  return function(d: any): void {
+    console.log('mouse up');
+    d3.event.stopPropagation();
+    d3.event.preventDefault();
+    let groupName = d3.select(this.parentNode).attr('group');
+    if (that.starting) {
+      const datum: any = d3.select(this.parentNode).datum();
+      const toId = datum.id;
+      groupName = d3.select(this.parentNode).attr('group');
+      const toPort = Object.keys(d)[0].split('.')[1];
+      if (groupName === 'outputs') {
+        if (that.starting.groupName === 'inputs') {
+          const newEdge = {
+            from: toId + '.' + toPort,
+            to: that.starting.from
+          };
+          const position = that.props.edges.findIndex(
+            (d: IEdge) => d.from === newEdge.from && d.to === newEdge.to
+          );
+          if (position >= 0) {
+            that.props.edges.splice(position, 1);
+          } else {
+            // only make the connection if it is valid
+            if (validConnection(that)(newEdge.from, newEdge.to)) {
+              that.props.edges.push(newEdge);
+            }
+          }
+          const output = that.configFile();
+          const jsonString = JSON.stringify(output);
+          that.drawLinks();
+          that.updateInputs(jsonString);
+          //that.props.setChartState({'edges': that.props.edges})
+          //let links = that.edgeData();
+          //link = that.link.data(links)
+          //    .join("line");
+        }
+      } else {
+        if (that.starting.groupName === 'outputs') {
+          const newEdge = {
+            from: that.starting.from,
+            to: toId + '.' + toPort
+          };
+          const position = that.props.edges.findIndex(
+            (d: IEdge) => d.from === newEdge.from && d.to === newEdge.to
+          );
+          if (position >= 0) {
+            that.props.edges.splice(position, 1);
+          } else {
+            // only make the connection if it is valid
+            if (validConnection(that)(newEdge.from, newEdge.to)) {
+              that.props.edges.push(newEdge);
+            }
+          }
+          const output = that.configFile();
+          const jsonString = JSON.stringify(output);
+          that.drawLinks();
+          that.updateInputs(jsonString);
+          //that.props.setChartState({'edges': that.props.edges})
+          // links = edges.map(edge_map);
+          // link = link.data(links)
+          //     .join("line");
+        }
+      }
+      that.starting = null;
+    }
+    that.drawCircles();
+  };
+}
+
+export function handleMouseDown(that: any) {
+  return function(d: any): void {
+    console.log('mouse down');
+    d3.event.stopPropagation();
+    d3.event.preventDefault();
+    let groupName = d3.select(this.parentNode).attr('group');
+    const portStr = Object.keys(d)[0];
+
+    //if (!that.starting) {
+    const datum: any = d3.select(this.parentNode).datum();
+    const fromId = datum.id;
+    groupName = d3.select(this.parentNode).attr('group');
+    const fromPort = Object.keys(d)[0].split('.')[1];
+    that.starting = { from: fromId + '.' + fromPort, groupName: groupName };
+
+    // }
+    if (groupName === 'inputs') {
+      //inputs, to in edges
+      const index = that.props.edges.findIndex((d: IEdge) => d.to === portStr);
+      if (index >= 0) {
+        that.starting = {
+          from: that.props.edges[index].from,
+          groupName: 'outputs'
+        };
+        that.props.edges.splice(index, 1);
+        const output = that.configFile();
+        const jsonString = JSON.stringify(output);
+        that.drawLinks();
+        that.updateInputs(jsonString);
+
+        //that.props.setChartState({'edges': that.props.edges})
+        // links = edges.map(edge_map);
+        // link = link.data(links)
+        //     .join("line");
+      }
+    } else {
+      //outputs, from in edges
+      const index = that.props.edges.findIndex(
+        (d: IEdge) => d.from === portStr
+      );
+      if (index >= 0) {
+        that.starting = {
+          from: that.props.edges[index].to,
+          groupName: 'inputs'
+        };
+        that.props.edges.splice(index, 1);
+        const output = that.configFile();
+        const jsonString = JSON.stringify(output);
+        that.drawLinks();
+        that.updateInputs(jsonString);
+        //that.props.setChartState({'edges': that.props.edges})
+        // links = edges.map(edge_map);
+        // link = link.data(links)
+        //     .join("line");
+      }
+    }
+    if (that.starting.groupName === 'outputs') {
+      that.starting['point'] = that.translateCorr(that.starting.from, true);
+    } else {
+      that.starting['point'] = that.translateCorr(that.starting.from, false);
+    }
+
+    that.drawCircles();
+  };
+}
---
+++ gquantlab/src/validator.ts
@@ -0,0 +1,82 @@
+import { IEdge } from './document';
+
+function valid(required: any, outputs: any): boolean {
+  const keys = Object.keys(required);
+
+  keys.forEach(d => {
+    if (!(d in outputs && required[d] === outputs[d])) {
+      return false;
+    }
+  });
+  return true;
+}
+
+export function validConnection(that: any) {
+  return function(from: string, to: string): any {
+    if (from === to) {
+      return true;
+    }
+    //canot connect to the port from the same node
+    if (from.split('.')[0] === to.split('.')[0]) {
+      return false;
+    }
+
+    // check whether from is input or output
+    const fromOutputPorts = that.outputPorts.has(from);
+    if (fromOutputPorts) {
+      // to has to be an input port
+      if (!that.inputPorts.has(to)) {
+        return false;
+      }
+      //from a output port, multiple connection from the same output port is allowed,
+      if (that.props.edges.findIndex((d: IEdge) => d.to === to) >= 0) {
+        // if there is already a connection to the input port, it is not valid
+        return false;
+      }
+
+      const toTypes = that.portTypes[to];
+      const fromTypes = that.portTypes[from];
+      const intersection = toTypes.filter((x: string) => fromTypes.includes(x));
+      if (intersection.length === 0) {
+        return false;
+      }
+
+      // make sure the requirement is met
+      if (from in that.outputColumns && to in that.inputRequriements) {
+        return valid(that.inputRequriements[to], that.outputColumns[from]);
+      } else if (
+        !(from in that.outputColumns) &&
+        to in that.inputRequriements
+      ) {
+        return valid(that.inputRequriements[to], {});
+      }
+    } else {
+      //from a input port, only single connection from input port is allowed
+      // to has to be output port
+      if (!that.outputPorts.has(to)) {
+        return false;
+      }
+      if (that.props.edges.findIndex((d: IEdge) => d.to === from) >= 0) {
+        // if there is already a connection to the input port, it is not valid
+        return false;
+      }
+      const toTypes = that.portTypes[to];
+      const fromTypes = that.portTypes[from];
+      const intersection = toTypes.filter((x: string) => fromTypes.includes(x));
+      if (intersection.length === 0) {
+        return false;
+      }
+
+      // make sure the requirement is met
+      if (to in that.outputColumns && from in that.inputRequriements) {
+        return valid(that.inputRequriements[from], that.outputColumns[to]);
+      } else if (
+        !(to in that.outputColumns) &&
+        from in that.inputRequriements
+      ) {
+        return valid(that.inputRequriements[from], {});
+      }
+    }
+    return true;
+  };
+}
