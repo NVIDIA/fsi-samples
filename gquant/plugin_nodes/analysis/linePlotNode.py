1a3fe48d6eead7e59540c0c00477fb5e96c1e31d
Yi Dong <doyend@gmail.com>
2020-07-31 22:46:04 -0400

    plot node is implemented

--- gquant/plugin_nodes/analysis/linePlotNode.py
+++ gquant/plugin_nodes/analysis/linePlotNode.py
@@ -1,14 +1,110 @@
 from gquant.dataframe_flow import Node
 from bqplot import Axis, LinearScale, DateScale, Figure, Lines, PanZoom
+from gquant.dataframe_flow.portsSpecSchema import ConfSchema
+from gquant.dataframe_flow.portsSpecSchema import (PortsSpecSchema,
+                                                   NodePorts)
 import cudf
 import dask_cudf
+import pandas as pd
 
 
 class LinePlotNode(Node):
 
+    def init(self):
+        self.INPUT_PORT_NAME = 'in'
+        self.OUTPUT_PORT_NAME = 'lineplot'
+        cols_required = {}
+        self.required = {
+            self.INPUT_PORT_NAME: cols_required
+        }
+
+    def conf_schema(self):
+        color_strings = ['black', 'yellow', 'blue',
+                         'red', 'green', 'orange',
+                         'magenta', 'cyan']
+        json = {
+            "title": "Line Plot Node Configuration",
+            "type": "object",
+            "description": """Plot the columns as lines""",
+            "properties": {
+                "points":  {
+                    "type": "number",
+                    "description": "number of data points for the chart"
+                },
+                "title":  {
+                    "type": "string",
+                    "description": "the plot title"
+                },
+                "lines": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "anyOf": [
+                            {
+                                "type": "object",
+                                "title": "Line Information",
+                                "properties": {
+                                    "column": {
+                                        "type": "string",
+                                    },
+                                    "label": {
+                                        "type": "string",
+                                    },
+                                    "color": {
+                                        "type": "string",
+                                        "enum": color_strings
+                                    }
+                                }
+                            },
+
+                        ]
+                    }
+                }
+            },
+            "required": ["points", "title", "lines"],
+        }
+        input_columns = self.get_input_columns()
+        if self.INPUT_PORT_NAME in input_columns:
+            col_inport = input_columns[self.INPUT_PORT_NAME]
+            enums = [col for col in col_inport.keys()]
+            first_item = json['properties']['lines']['items']['anyOf'][0]
+            first_item['properties']['column']['enum'] = enums
+            ui = {}
+            return ConfSchema(json=json, ui=ui)
+        else:
+            ui = {
+                "column": {"ui:widget": "text"}
+            }
+            return ConfSchema(json=json, ui=ui)
+
+    def ports_setup(self):
+        types = [cudf.DataFrame,
+                 dask_cudf.DataFrame,
+                 pd.DataFrame]
+        port_type = PortsSpecSchema.port_type
+        input_ports = {
+            self.INPUT_PORT_NAME: {
+                port_type: types
+            }
+        }
+        output_ports = {
+            self.OUTPUT_PORT_NAME: {
+                port_type: Figure
+            }
+        }
+
+        input_connections = self.get_connected_inports()
+        if (self.INPUT_PORT_NAME in input_connections):
+            determined_type = input_connections[self.INPUT_PORT_NAME]
+            # connected
+            return NodePorts(inports={self.INPUT_PORT_NAME: {
+                port_type: determined_type}},
+                outports=output_ports)
+        else:
+            return NodePorts(inports=input_ports, outports=output_ports)
+
     def columns_setup(self):
-        self.required = {"datetime": "date"}
-        self.retentation = {}
+        return {self.OUTPUT_PORT_NAME: {}}
 
     def process(self, inputs):
         """
@@ -26,7 +122,7 @@ class LinePlotNode(Node):
         Figure
         """
 
-        input_df = inputs[0]
+        input_df = inputs[self.INPUT_PORT_NAME]
 
         num_points = self.conf['points']
         stride = max(len(input_df) // num_points, 1)
@@ -58,17 +154,4 @@ class LinePlotNode(Node):
             lines.append(line)
         new_fig = Figure(marks=lines, axes=[yax, xax],
                          title=self.conf['title'], interaction=panzoom_main)
-        return new_fig
-
-
-if __name__ == "__main__":
-    from gquant.dataloader.csvStockLoader import CsvStockLoader
-    from gquant.transform.averageNode import AverageNode
-    from gquant.analysis.outCsvNode import OutCsvNode
-
-    loader = CsvStockLoader("id0", {}, True, False)
-    df = loader([])
-    vf = AverageNode("id1", {"column": "volume"})
-    df2 = vf([df])
-    o = OutCsvNode("id3", {"path": "o.csv"})
-    o([df2])
+        return {self.OUTPUT_PORT_NAME: new_fig}
