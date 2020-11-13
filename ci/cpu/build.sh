ae9fc643f9a596b28343056bc583de0d11987836
Mark Sadang <marksadang@engineer.com>
2020-11-12 21:52:26 -0800

    Updated upload.sh and build.sh

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -12,47 +12,49 @@ function gpuci_logger() {
 
 # Set path and build parallel level
 export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
+export PARALLEL_LEVEL=${PARALLEL_LEVEL:-4}
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
 
-# Switch to project root; also root of repo checkout
-cd $WORKSPACE
-
-# Get latest tag and number of commits since tag
-export GIT_DESCRIBE_TAG=`git describe --abbrev=0 --tags`
-export GIT_DESCRIBE_NUMBER=`git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count`
+# Determine CUDA release version
+export CUDA_REL=${CUDA_VERSION%.*}
 
 # Setup 'gpuci_conda_retry' for build retries (results in 2 total attempts)
 export GPUCI_CONDA_RETRY_MAX=1
 export GPUCI_CONDA_RETRY_SLEEP=30
 
+# Switch to project root; also root of repo checkout
+cd $WORKSPACE
+
+# If nightly build, append current YYMMDD to version
+if [[ "$BUILD_MODE" = "branch" && "$SOURCE_BRANCH" = branch-* ]] ; then
+  export VERSION_SUFFIX=`date +%y%m%d`
+fi
+
 ################################################################################
 # SETUP - Check environment
 ################################################################################
 
-gpuci_logger "Get env"
+gpuci_logger "Check environment variables"
 env
 
 gpuci_logger "Activate conda env"
 . /opt/conda/etc/profile.d/conda.sh
 conda activate rapids
 
-gpuci_logger "Check versions"
+gpuci_logger "Check compiler versions"
 python --version
 $CC --version
 $CXX --version
+
+gpuci_logger "Check conda environment"
 conda info
 conda config --show-sources
 conda list --show-channel-urls
 
 # FIX Added to deal with Anancoda SSL verification issues during conda builds
 conda config --set ssl_verify False
-
-# If nightly build, append current YYMMDD to version
-if [[ "$BUILD_MODE" = "branch" && "$SOURCE_BRANCH" = branch-* ]] ; then
-  export VERSION_SUFFIX=`date +%y%m%d`
-fi
 
 ################################################################################
 # BUILD - Conda package build
@@ -65,4 +67,4 @@ gpuci_conda_retry build conda/recipes/gquant --python=${PYTHON}
 ################################################################################
 
 gpuci_logger "Upload conda pkg"
-source ci/cpu/upload-anaconda.sh
+source ci/cpu/upload.sh
---
+++ ci/cpu/upload.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Adopted from https://github.com/tmcdonell/travis-scripts/blob/dfaac280ac2082cd6bcaba3217428347899f2975/update-accelerate-buildbot.sh
+
+set -e
+
+# Setup 'gpuci_retry' for upload retries (results in 4 total attempts)
+export GPUCI_RETRY_MAX=3
+export GPUCI_RETRY_SLEEP=30
+
+# Set default label options if they are not defined elsewhere
+export LABEL_OPTION=${LABEL_OPTION:-"--label main"}
+
+# Skip uploads unless BUILD_MODE == "branch"
+if [ ${BUILD_MODE} != "branch" ]; then
+  echo "Skipping upload"
+  return 0
+fi
+
+# Skip uploads if there is no upload key
+if [ -z "$MY_UPLOAD_KEY" ]; then
+  echo "No upload key"
+  return 0
+fi
+
+CUDA_REL=${CUDA_VERSION%.*}
+
+SOURCE_BRANCH=master
+
+LABEL_OPTION="--label main"
+echo "LABEL_OPTION=${LABEL_OPTION}"
+
+# Restrict uploads to master branch
+if [ ${GIT_BRANCH} != ${SOURCE_BRANCH} ]; then
+  echo "Skipping upload"
+  return 0
+fi
+
+################################################################################
+# SETUP - Get conda file output locations
+################################################################################
+
+gpuci_logger "Get conda file output locations"
+export GQAUNT_FILE=`conda build conda/recipes/gquant --python=$PYTHON --output`
+
+################################################################################
+# UPLOAD - Conda packages
+################################################################################
+
+if [ "$UPLOAD_GQUANT" == "1" ]; then
+  test -e ${GQAUNT_FILE}
+  echo "Upload gquant"
+  echo ${GQAUNT_FILE}
+  gpuci_retry anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} ${LABEL_OPTION} --force ${GQAUNT_FILE}
+fi
